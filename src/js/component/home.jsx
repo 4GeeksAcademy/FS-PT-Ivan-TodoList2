daf2c9876034f183e5eaba53c4cdea19d47efc2c
Ivan Perez <pumi0751@gmail.com>
2024-11-07 10:55:07 +0000

    feat: finish

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [email, setEmail] = useState('');
+	const [emailList, setEmailList] = useState([]);
+
+	//functions
+	const handleSubmit = e => {
+		e.preventDefault();
+		setEmailList([...emailList, email]);
+	}
+
+	const handleChange = e => {
+		setEmail(e.target.value);
+	}
+
+	const handleDelete = (index) => {
+		setEmailList(emailList.filter((el, i) => i != index))
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="page">
+			<div className="text-center">
+				<form onSubmit={handleSubmit} className="form">
+					<div className="subscribe">
+						<p>SUBSCRIBE</p>
+						<input
+							placeholder="Your e-mail"
+							className="subscribe-input"
+							name="email"
+							type="email"
+							value={email}
+							onChange={handleChange}
+							required
+						/>
+						<br />
+						<input className="submit-btn d-flex" type="submit" value="Submit" />
+					</div>
+				</form>
+
+				<ul className="emailList">
+					{emailList.length > 0
+						? emailList.map((email, i) => (
+							<li key={i}>
+								{email}
+								<span onClick={() => handleDelete(i)}>
+									<i className="fa-solid fa-xmark"></i>
+								</span>
+							</li>
+						))
+						: <li>Add some emails</li>
+					}
+				</ul>
+				<p className="itemCount">{emailList.length} item left</p>
+			</div>
 		</div>
+
 	);
 };
 
